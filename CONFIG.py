7e80c4778cde55f54b1c454609646f0a6f856e92
KenMLCV <41591993+KenMLCV@users.noreply.github.com>
2023-09-27 21:48:41 -0400

    Create CONFIG.py

---
+++ CONFIG.py
@@ -0,0 +1,12 @@
+gis = {
+    "api_key": "AIzaSyDo4ykwhCl28rl8z95vI6vacm-dhCxK3v0",
+    "project_id": "fe34a4fad27fd9404",
+}
+
+gcp = {
+    "conn": "opencv-examples:us-central1:opencv-db",
+    "user": "root",
+    "password": "y3t@n0th3rP@ssw0rd",
+    "driver": "pymysql",
+    "db": "cereal_box_data"
+}
